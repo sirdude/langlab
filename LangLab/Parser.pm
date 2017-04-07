2d1698ed975e7929f273450fefa38472a574c252
Kent Mein <mein@cs.umn.edu>
2017-04-07 09:10:42 -0500

    Added a function to grab an Ident.

--- LangLab/Parser.pm
+++ LangLab/Parser.pm
@@ -16,7 +16,8 @@ my ($current, $treesize, @tree);
 sub get_single_comment {
 	my ($node, $done);
 
-	if ($tree[$current]->{'value'} ne "#") {
+	if (($tree[$current]->{'value'} ne "#") &&
+		($tree[$current]->{'value'} ne "--")) {
 		print "Called get_single_comment, and first value is not " .
 			" the start of a single line comment.\n";
 		return 0;
@@ -84,8 +85,66 @@ sub get_multi_comment {
 	return $node;
 }
 
+sub is_letter {
+	my ($char) = @_;
+
+	if (($char >= 'a') && ($char <= 'z')) {
+		return 1;
+	}
+	if (($char >= 'A') && ($char <= 'Z')) {
+		return 1;
+	}
+
+	return 0;
+}
+
+sub is_digit {
+	my ($char) = @_;
+
+	if ($char eq "1") {
+	} elsif ($char eq "2") {
+	} elsif ($char eq "3") {
+	} elsif ($char eq "4") {
+	} elsif ($char eq "5") {
+	} elsif ($char eq "6") {
+	} elsif ($char eq "7") {
+	} elsif ($char eq "8") {
+	} elsif ($char eq "9") {
+	} elsif ($char eq "0") {
+	} else {
+		return 0;
+	}
+
+	return 1;
+}
+
 sub get_id {
-	my $node;
+	my ($node, $done);
+
+	$done = 0;
+
+	if (!is_letter($tree[$current]->{'value'})) {
+		print "Error: " .  $node->['file'] .
+			" line " . $node->['pos'] .
+			"Expected ID which starts with a Letter.\n";
+		return 0;
+	}
+
+	$node->{'value'} = "";
+	$node->{'type'} = "ident";
+	$node->{'file'} = $tree[$current]->{'value'};
+	$node->{'pos'} = $tree[$current]->{'pos'};
+
+	while(!$done && (is_letter($tree[$current]->{'value'}) ||
+		is_digit($tree[$current]->{'value'}))) {
+		$node->{'value'} = $node->{'value'} . 
+			$tree[$current]->{'value'};
+		$current = $current + 1;
+
+		if ($current > $treesize) {
+			$done = 1;
+		}
+	}
 
 	return $node;
 }
@@ -116,7 +175,7 @@ sub get_doublequotestring {
 			$tree[$current]->{'value'};
 
 		$current = $current + 1;
-		if ($current < $treesize) {
+		if ($current > $treesize) {
 			$done = 1;
 		}
 	}
