95c300b8ff2347c2761885208db643cbe5324f9c
Kent Mein <mein@cs.umn.edu>
2017-06-06 11:36:13 -0500

    Work on reading/writing token file,
    	also moved grammar files to a subdir.

--- dotok.pl
+++ dotok.pl
@@ -3,10 +3,14 @@
 use strict;
 use warnings;
 
+my (@tokens);
+
 sub valid_type {
 	my ($input) = @_;
 
 	if ($input eq "delimiter") {
+		return 1;
+	} elsif ($input eq "terminal") {
 		return 1;
 	} elsif ($input eq "string") {
 		return 1;
@@ -31,14 +35,24 @@ sub read_file {
 
 		if ($line =~ /^#(.*)/) {
 			# skip comments
-		} elsif ($line =~ /^(\s)+ (.*)$/) {
+		} elsif ($line =~ /^$/) {
+			# skip blank lines
+		} elsif ($line =~ /^(\w+) (.*)$/) {
 			$type = $1;
 			$value = $2;
 
 			if (!valid_type($type)) {
 				print "Error line $c: invalid type $type\n";
 			} else {
-				# XXX Need to set values here...
+				my $node;
+				$node->{"file"} = $infile;
+				$node->{"line"} = $c;
+				$node->{"column"} = 0; # XXX This should be figured out?
+				$node->{"type"} = $type;
+				$node->{"value"} = $value;
+
+				push(@tokens, $node);
+				
 			}
 		} else {
 			print "Error line $c: $line\n";
@@ -50,6 +64,20 @@ sub read_file {
 
 sub write_file {
 	my ($outfile) = @_;
+	my ($fh);
+
+	open($fh, ">", $outfile) or die "Unable to open $outfile for writing.\n";
+
+	if (!$fh) {
+		return 0;
+	}
+	foreach my $node (@tokens) {
+		print $fh $node->{"type"} . " " . $node->{"value"} . "\n";
+	}
+
+	close($fh);
+
+	return 1;
 }
 
 sub usage {
@@ -65,5 +93,5 @@ if (!$filename || $filename eq "") {
 	usage();
 } else {
 	read_file($filename);
-	# write_file($filename . ".new");
+	write_file($filename . ".new");
 }
