5d54ed69367dc49f278dfc5bfcebd9393896f9eb
Kent Mein <mein@cs.umn.edu>
2017-06-06 23:40:43 -0500

    Renamed our code to match what it does.

--- printit.pl
+++ printit.pl
@@ -2,37 +2,78 @@
 
 use strict;
 use warnings;
+my %rules;
 
-use lib ".";
-use LangLab::Symbols;
-use LangLab::Parser;
+sub insert_rule {
+	my ($lhs, $rhs) = @_;
 
-use Getopt::Long;
+	if (exists($rules{$lhs})) {
+		my @tmp = @{$rules{$lhs}};
+		foreach my $i (@tmp) {
+			if ($rhs eq $i) {
+				print "Error Duplcate rule: $lhs ::= $rhs\n";
+				return 0;
+			}
+		}
+	}
 
-my (%Options);
+	push(@{$rules{$lhs}}, $rhs);
 
-sub usage {
-	print "\tUsage: $0 [--debug] filenames(s)\n";
-	print "Take a grammar and print out code to generate that language " .
-		"from " a parse tree.\n";
 	return 1;
+}
+
+sub generate_is {
+	my ($value) = @_;
+
+	print "sub is_" . $value . "{\n";
+	print "\tmy (\$ptr) = \@_;\n\n";
+
+	my @tmp = @{$rules{$value}};
+	foreach my $x (@tmp) {
+		print "\tif (\$ptr eq $x) {\n";
+		print "\t\treturn 1;\n";
+		print "\t}\n";
+	}
+
+	print "\n\treturn 0;\n";
+
+	print "\n}\n\n";
+}
+
+sub generate_do {
+	my ($value) = @_;
+
+	print "sub do_" . $value . "{\n";
+	print "\tmy (\$ptr) = \@_;\n\n";
+
+	print "\tif (!is_" . $value . "(\$ptr) {\n";
+	print "\t\tprint \"Error!!! Expected something else here: XXX\\n\";\n";
+	print "\t\treturn 0;\n";
+	print "\t}\n";
+
+	print "XXX Do consume here\n";
+
+	print "\n\treturn 0;\n";
+
+	print "\n}\n\n";
 }
 
 sub generate_code {
-	my (@tree) = @_;
-
-	foreach my $i (@tree) {
+	foreach my $i (keys %rules) {
+		generate_is($i);
+		generate_do($i);
 	}
 }
 
-GetOptions(\%Options, "debug");
-
-
-if ($#ARGV < 0) {
-	usage();
-	return 1;
-}
-
-#  XXX Need to load the parse tree here.
+insert_rule("Digit", "0");
+insert_rule("Digit", "1");
+insert_rule("Digit", "2");
+insert_rule("Digit", "3");
+insert_rule("Digit", "4");
+insert_rule("Digit", "5");
+insert_rule("Digit", "6");
+insert_rule("Digit", "7");
+insert_rule("Digit", "8");
+insert_rule("Digit", "9");
 
 generate_code();
