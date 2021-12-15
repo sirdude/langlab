db868d576bd0b91b4e5514b8ced5722022807eb4
Kent <mein@umn.edu>
2021-12-15 13:34:34 -0600

    Added a basic symbol table.

---
+++ lib/symbol_table.pm
@@ -0,0 +1,96 @@
+package symbol_table;
+
+use warnings;
+use base 'Exporter';
+
+our @EXPORT = qw(lookup_value lookup_type intable insert_symbol
+	dump_table);
+
+sub new {
+	my $class = shift;
+	my $self = {};
+	$self->{'symtable'} = ();
+
+	bless $self, $class;
+	return $self;
+}
+
+# Simple symbol table functions
+sub lookup_value {
+	my ($self, $var) = @_;
+
+	if (exists($self->{'symtable'}{$var})) {
+		return $self->{'symtable'}{$var}->{'value'};
+	}
+
+	return "";
+}
+
+sub lookup_type {
+	my ($self, $var) = @_;
+
+	if (exists($self->{'symtable'}{$var})) {
+		return $self->{'symtable'}{$var}->{'type'};
+	}
+	return '';
+}
+
+sub intable {
+	my ($self, $var) = @_;
+
+	if (exists($self->{'symtable'}{$var})) {
+		$self->{'symtable'}{$var}->{'count'} = $self->{'symtable'}{$var}->{'count'} + 1;
+		return 1;
+	}
+
+	return 0;
+}
+
+sub insert_symbol {
+	my ($self, $sym, $type, $val) = @_;
+	my %tmp;
+
+#	debug('insert_symbol: Sym: ' . $sym . ' Type: ' .
+#		$type . ' Val: ' . $val);
+
+	if (intable($sym)) {
+		error('Duplicate entry: ' . $sym);
+		return 0;
+	}
+
+	$tmp{'value'} = $val;
+	$tmp{'type'} = $type;
+	$tmp{'count'} = 1;
+	$self->{'symtable'}{$sym} = \%tmp;
+
+	return 1;
+}
+
+sub dump_table {
+	my ($self, $tmp, $val);
+
+	print "Symbol Table Dump:\n";
+	foreach my $i (keys %{$self->{'symtable'}}) {
+		$tmp = $self->{'symtable'}{$i};
+
+		if (exists($tmp->{'value'})) {
+			$val = $tmp->{'value'};
+		} else {
+			$val = 'null';
+		}
+
+		if ($tmp->{'type'} eq 'keyword') {
+			print '\tSymbol: ' . $i . ': type: ' . $tmp->{'type'} .
+				" count: " . $$tmp->{'count'} . "\n";
+		} else {
+			print "\tSymbol: " . $i . ': type: ' . $tmp->{'type'} .
+				' value: ' . $val . ' count: ' . $tmp->{'count'} .
+				"\n";
+		}
+	}
+	print "\n\n";
+
+	return 1;
+}
+
+1;
---
+++ tests/02symbol_table.t
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib "./lib";
+use lib "../lib";
+
+use symbol_table;
+use tests;
+
+my ($testast);
+
+sub test_sym_table {
+	is($testast->insert_symbol('x','int',5), 1, 'Insert symbol x.');
+	is($testast->intable('x'), 1, 'Is x in our table?');
+	is($testast->intable('y'), 0, 'Is y in our table?');
+	is($testast->lookup_value('x'), 5, 'Get the value of x.');
+	is($testast->lookup_value('y'), 0, 'Get the value of y.');
+	is($testast->lookup_type('x'), 'int', 'Get the value of x.');
+	is($testast->lookup_type('y'), '', 'Get the value of y.');
+
+	return 1;
+}
+
+sub main {
+	$testast = symbol_table->new();
+	init_tests();
+	test_sym_table();
+	test_summary();
+
+	return 1;
+}
+
+main();
