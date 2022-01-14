7596d0530b3aa6d119d700039fe8c86c6e4a29d7
Kent <mein@umn.edu>
2022-01-14 16:16:21 -0600

    Removed a stray print statement.

--- lib/sweet/struct_block.pm
+++ lib/sweet/struct_block.pm
@@ -39,7 +39,6 @@ sub get {
 	$tmp = $ast->consume('{');
 
 	while (!$ast->match('}') && !$done) {
-print "Calling get statement\n";
 	    $tmp = {};
 		if (struct_statement::get($ast, $tmp)) {
 			push(@{$node->{'data'}}, $tmp);
