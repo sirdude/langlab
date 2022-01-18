f4338196df0b4181580712664723d7f54066df01
Kent <mein@umn.edu>
2022-01-18 12:59:51 -0600

    Expand tests to cover arrays and hashes.  May still need some work.

--- lib/tests.pm
+++ lib/tests.pm
@@ -3,6 +3,7 @@ package tests;
 use warnings;
 use Term::ANSIColor;
 use base 'Exporter';
+use Scalar::Util qw(reftype);
 
 our @EXPORT = qw(add_test add_success total_tests total_success is
 	init_tests test_summary);
@@ -29,12 +30,79 @@ sub total_success {
 	return $success;
 }
 
+sub is_hash {
+	my ($input) = @_;
+
+	if (ref($input) && (reftype($input) eq "HASH")) {
+		return 1;
+	}
+	return 0;
+}
+
+sub is_array {
+	my ($input) = @_;
+
+	if (ref($input) && (reftype($input) eq "ARRAY")) {
+		return 1;
+	}
+	return 0;
+}
+
+sub compare_hash {
+	my ($h1, $h2) = @_;
+	my %rhash_1 = %$a;
+	my %rhash_2 = %$b;
+
+	my $hash_2_line = undef;
+	my $hash_1_line = undef;
+
+	foreach my $key ( keys(%rhash_2) ) {
+		if ( exists( $rhash_1{$key} ) ) {
+			if ( $rhash_1{$key} ne $rhash_2{$key} ) {
+				return 0;
+			}
+	 	} else {
+			return 0;
+   		}
+	}
+
+	foreach my $comp_key ( keys %rhash_1 ) {
+		if ( !exists( $rhash_2{$comp_key} ) ) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+sub compare_array {
+	my ($h1, $h2) = @_;
+
+	my $len = length($h1);
+
+	if ($len ne length($h2)) {
+		return 0;
+	}
+
+	my $x = 0;
+	while ($x < $len) {
+		if ($h1[$x] ne $h2[$x]) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
 sub is {
 	my ($functioncall, $expected, $text) = @_;
 
 	add_test();
 
-	if (!$functioncall) {
+	if (is_hash($functioncall) || is_hash($expected)) {
+		return compare_hash($functioncall, $expected);
+	} elsif (is_array($functioncall) || is_array($expected)) {
+		return compare_array($functioncall, $expected);
+	} elsif (!$functioncall) {
 		if (!$expected) {
 			add_success();
 			print color('bold green') . "ok " . color('reset') . total_tests() . " - $text\n";
