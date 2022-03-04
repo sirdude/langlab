2c9a91bce2846352649af0e8d11f87b6be48c1fd
Kent <mein@umn.edu>
2022-03-04 09:53:54 -0600

    Flesh out binop and unop.

--- lib/sweet/out_xml/binop.pm
+++ lib/sweet/out_xml/binop.pm
@@ -24,8 +24,25 @@ sub is {
 sub output {
 	my ($self, $data) = @_;
 
-	# XXX do this
+	$data .= "<binop>\n";
+	$data .= "<optype>" . $self->{'type'} . "</optype>\n";
 
+	if (!ident::is($self->{'lhs'})) {
+		$self->error("Invalid package: " . Dumper($self->{'data'});
+		return 0;
+	}
+	if (!ident::output($self->{'lhs'}, \$output) {
+		return 0;
+	}
+	if (!expression::is($self->{'rhs'})) {
+		$self->error("Invalid package: " . Dumper($self->{'data'});
+		return 0;
+	}
+	if (!expression::output($self->{'rhs'}, \$output) {
+		return 0;
+	}
+
+	$data .= "</binop>\n";
 }
 
 1;
--- lib/sweet/out_xml/unop.pm
+++ lib/sweet/out_xml/unop.pm
@@ -21,7 +21,18 @@ sub is {
 sub output {
 	my ($self, $data) = @_;
 
-# XXX do this...
+	$data .= "<unop>\n";
+	$data .= "<optype>" . $self->{'type'} . "</optype>\n";
+
+	if (!expression::is($self->{'data'})) {
+		$self->error("Invalid package: " . Dumper($self->{'data'});
+		return 0;
+	}
+	if (!expression::output($self->{'data'}, \$output) {
+		return 0;
+	}
+
+	$data .= "</unop>\n";
 
 }
 
